f872b4dbeebe555055b7f74735b11d1311ab7eb9
Tim Hacker <tim.hacker@trueclarity.co.uk>
2011-11-28 13:27:14 +0000

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackerPhotography::Application.config.secret_token = '3698de58c6bd36a611d2ef7909e10ad9560a81afad36f4a6303e33c861689e33322ad121325d0ee12a9628a13e2f14feef1ff1baeac9e20e502b2be7b27e4c14'
