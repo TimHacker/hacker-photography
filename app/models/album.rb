01d7aa46c204a79de3a7d632c98c23e90b6b1ba3
Tim Hacker <tim.hacker@trueclarity.co.uk>
2011-12-13 14:55:41 +0000

    cleaned up photos and albums

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,8 +2,8 @@ class Album < ActiveRecord::Base
 	
 	has_many :photos
 	
-	def album_photos
-		Photo.where("album_id = ?", id)
-	end
+	#def album_photos
+	#	Photo.where("album_id = ?", id)
+	#end
 
 end
