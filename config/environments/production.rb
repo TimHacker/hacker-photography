b21ef5fc88b95e521ba366028f088c1a3c776584
Tim Hacker <tim.hacker@trueclarity.co.uk>
2012-08-27 19:31:31 +0100

    both precompiled css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,8 +23,8 @@ HackerPhotography::Application.configure do
   config.consider_all_requests_local = true
   config.log_level = :debug
   
-  config.assets.precompile += %w( 'Application-Screen.css', 'Application-IE.css'  )
-
+  config.assets.precompile += %w( Application-Screen.css )
+  config.assets.precompile += %w( Application-IE.css )
   
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
